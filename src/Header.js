f57be6d007c66512b6a9e7dc7e87f1d0f1f2b1db
Zakyboss <Boochimo8@gmail.com>
2024-12-24 21:40:57 +0300

    Completed the header

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+
+function Header(){
+    return (
+        <header>
+        <h1 className='header'>Welcome To My Car Website</h1>
+        <h3 className='theCars'>The cars!</h3>
+        </header>
+    )
+}
+export default Header
