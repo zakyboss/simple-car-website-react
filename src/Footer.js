d04e07c0eb356955f6b40ec8acebdbffdd688a68
Zakyboss <Boochimo8@gmail.com>
2024-12-24 22:11:20 +0300

    Footer completed

--- src/Footer.js
+++ src/Footer.js
@@ -1,19 +1,43 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-function Footer (){
-return (
-<div>
-  <footer> 
-    <hr/>
-    <h3>Made With Love by Yours Truly 💗</h3>
-    <div className='btns'>
-<button>Github</button>
-<button>Instagram</button>
-<button>LinkedIn</button>
-</div>
-  </footer>
 
-</div>
-)
+function Footer() {
+  return (
+    <div>
+      <footer>
+        <hr />
+        <h3>Made With Love by Yours Truly 💗</h3>
+        <div className="btns">
+          <button>
+            <a 
+              href="https://github.com/zakyboss" 
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
+              Github
+            </a>
+          </button>
+          <button>
+            <a 
+              href="https://instagram.com/grimphsyco" 
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
+              Instagram
+            </a>
+          </button>
+          <button>
+            <a 
+              href="https://www.linkedin.com/in/zakariya-mohamed-774531284" 
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>
+          </button>
+        </div>
+      </footer>
+    </div>
+  );
 }
+
 export default Footer;
