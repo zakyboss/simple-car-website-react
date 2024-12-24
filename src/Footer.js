a0eb6f4d3dcf03919c07a20639c29668c59af4b2
Zakyboss <Boochimo8@gmail.com>
2024-12-24 22:05:18 +0300

    Added Footer

---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+function Footer (){
+return (
+<div>
+  <footer> 
+    <hr/>
+    <h3>Made With Love by Yours Truly 💗</h3>
+    <div className='btns'>
+<button>Github</button>
+<button>Instagram</button>
+<button>LinkedIn</button>
+</div>
+  </footer>
+
+</div>
+)
+}
+export default Footer;
